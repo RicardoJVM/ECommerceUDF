9a775795ebaffc18a86ad7dce5972f85bd3b674b
Ricardo Valencia <rvalencia@t2company.com.uy>
2026-09-18 10:26:43 -0300

    Initial Commit

---
+++ ECommerceUDF/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ECommerceUDF
+//
+//  Created by Ricardo Valencia on 18/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ECommerceUDF/ECommerceUDFApp.swift
@@ -0,0 +1,17 @@
+//
+//  ECommerceUDFApp.swift
+//  ECommerceUDF
+//
+//  Created by Ricardo Valencia on 18/9/26.
+//
+
+import SwiftUI
+
+@main
+struct ECommerceUDFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
